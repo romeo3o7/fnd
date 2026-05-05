ba361b05bd2328baffc475f88125f76a1e4d36b2
romeo3o7 <romeo3o7@protonmail.com>
2026-05-05 14:51:10 +0300

    formating and logic

--- update.c
+++ update.c
@@ -21,21 +21,21 @@ int update() {
     printf("update found!\n");
     printf("would you like an offline or online update?\n");
     printf("offline if the update includes: kernel,libc,systemd,mesa.\n");
-    printf("'o' for online and 'f' for offline: ");
     char *online[] = { "sudo" , "dnf" , "upgrade" , NULL} , *offline[] = {"sudo" , "dnf" , "upgrade" , "--offline" , NULL};
     char **update;
     char buffer[8];
     int reboot = 0;
     while(1) {
+        printf("'o' for online and 'f' for offline: ");
+        fflush(stdout);
         char *input= fgets(buffer, sizeof(buffer) , stdin);
         if (input != NULL && (buffer[0] == 'o' || buffer[0] == 'f')) { // if pointer input is not null and first value of buffer is either 'o' or 'f'
-       if (buffer[0] == 'f') {
-           update = offline;
-           reboot = 1;
-       }
-       else update = online;
-       fflush(stdout);
-       break;
+            if (buffer[0] == 'f') {
+                update = offline;
+                reboot = 1;
+            }
+            else update = online;
+            break;
         }
     }
     int updateStatus = task("sudo",update);
@@ -65,16 +65,14 @@ int clearOrphans() {
     return 0;
 }
 int offlineActions() {
-    printf("Offline transactions are avaliable would you like to update now? y/n: ");
-    fflush(stdout);
     char buffer[8];
     while (1) {
-    char *input = fgets(buffer, sizeof(buffer),stdin);
-    if (input == NULL) {
-        return -1;
-    }
-    if ( buffer[0] != 'y' && buffer[0] != 'n') continue;
-    break;
+        printf("Offline transactions are avaliable would you like to update now? y/n: ");
+        fflush(stdout);
+        char *input = fgets(buffer, sizeof(buffer),stdin);
+        if (input == NULL)     return -1;
+        if (buffer[0] == 'n')  return 1;
+        if (buffer[0] == 'y')  break;
     }
     char * argument[] = {"sudo", "dnf5" , "offline" , "reboot" , NULL};
     return task("sudo",argument);
@@ -82,7 +80,7 @@ int offlineActions() {
 
 int invoke_update() {
     int checkUpdateStatus = checkUpdate();
-    if (checkUpdateStatus != 0) exit(1);
+    if (checkUpdateStatus != 0) exit(0);
     int updateStaus = update();
     if (updateStaus == -1) return 1;
     int cacheStatus = clearCache();
